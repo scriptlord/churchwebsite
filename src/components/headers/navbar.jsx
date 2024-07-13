50704909ac40e77f5f4a5bc11b4a65732482f264
Chukwudi Eze <chukwudieze97@gmail.com>
2024-07-13 19:25:06 +0100

    refix navbar

--- src/components/headers/navbar.jsx
+++ src/components/headers/navbar.jsx
@@ -1,146 +1,49 @@
-// import React, { useState } from "react";
-// import styles from "./Navbar.module.scss";
-
-// function Navbar() {
-//   const [active, setActive] = useState(styles.nav__menu);
-//   const [icon, setIcon] = useState(styles.nav__toggler);
-
-//   const navToggle = () => {
-//     if (active === styles.nav__menu) {
-//       setActive(`${styles.nav__menu} ${styles.nav__active}`);
-//     } else setActive(styles.nav__menu);
-
-//     // Icon Toggler
-//     if (icon === styles.nav__toggler) {
-//       setIcon(`${styles.nav__toggler} ${styles.toggle}`);
-//     } else setIcon(styles.nav__toggler);
-//   };
-
-//   return (
-//     <nav className={styles.nav}>
-//       <a href="#" className={styles.nav__brand}>
-//         herdoy
-//       </a>
-//       <ul className={active}>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             Home
-//           </a>
-//         </li>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             About
-//           </a>
-//         </li>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             Schedule
-//           </a>
-//         </li>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             FAQS
-//           </a>
-//         </li>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             Testimonies
-//           </a>
-//         </li>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             Contact Us
-//           </a>
-//         </li>
-//         <li className={styles.nav__item}>
-//           <a href="#" className={styles.nav__link}>
-//             Donate
-//           </a>
-//         </li>
-//       </ul>
-//       <div onClick={navToggle} className={icon}>
-//         <div className={styles.line1}></div>
-//         <div className={styles.line2}></div>
-//         <div className={styles.line3}></div>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navbar;
-
-
-
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
-import styles from "./Navbar.module.scss";
 
-function Navbar() {
-  const [active, setActive] = useState(false);
-  const navToggle = () => setActive(!active);
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggle = () => setIsActive(!isActive);
 
   return (
-    <nav className={styles.nav}>
-      <div className={styles.nav__top}>
-        <a href="#" className={styles.nav__brand}>
-          <img src="logo.png" alt="JEM Logo" className={styles.nav__logo} />
-        </a>
-        <div className={styles.nav__actions}>
-          <button className={styles.nav__volunteer}>Volunteer</button>
-          <button className={styles.nav__donate}>Donate</button>
-          <div className={styles.nav__socials}>
-            <a href="#" className={styles.nav__social}><i className="fab fa-facebook"></i></a>
-            <a href="#" className={styles.nav__social}><i className="fab fa-youtube"></i></a>
-            <a href="#" className={styles.nav__social}><i className="fab fa-tiktok"></i></a>
-            <a href="#" className={styles.nav__social}><i className="fab fa-spotify"></i></a>
+    <nav className="navbar-wrapper">
+      <div className="d-flex justify-content-between align-items-center">
+        <span className={`d-inline-block position-relative logo`}>
+          <Image src="/" alt="" fill />
+        </span>
+        <button className="d-lg-none menu-button" onClick={toggle}>
+          {isActive ? <i class="bi bi-x"></i> : <i class="bi bi-list"></i>}
+        </button>
+        <div className="d-none d-lg-flex align-items-center gap-3">
+          <button className="nav-button">Volunteer</button>
+          <button className="nav-button">Donate</button>
+          <div>
+            <Link href="#" className="me-3">
+              <i className="bi bi-facebook"></i>
+            </Link>
+            <Link href="#">
+              <i className="bi bi-youtube"></i>
+            </Link>
           </div>
         </div>
-        <div onClick={navToggle} className={`${styles.nav__toggler} ${active ? styles.toggle : ""}`}>
-          <div className={styles.line1}></div>
-          <div className={styles.line2}></div>
-          <div className={styles.line3}></div>
-        </div>
       </div>
-      <div className={`${styles.nav__bottom} ${active ? styles.nav__active : ""}`}>
-        <ul className={styles.nav__menu}>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Home
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              About
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Schedule
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              FAQs
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Resources
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Testimonies
-            </a>
-          </li>
-          <li className={styles.nav__item}>
-            <a href="#" className={styles.nav__link}>
-              Contact Us
-            </a>
-          </li>
-        </ul>
+      <div
+        className={`d-flex justify-content-lg-end align-items-center align-items-lg-start flex-column flex-lg-row gap-4 end-0 links-wrapper ${
+          isActive ? "active" : ""
+        }`}
+      >
+        <Link href="#">Home</Link>
+        <Link href="#">Home</Link>
+        <Link href="#">Home</Link>
+        <Link href="#">Home</Link>
+        <Link href="#">Home</Link>
+        <button className="d-inline d-lg-none nav-button">Volunteer</button>
+        <button className="d-inline d-lg-none nav-button">Donate</button>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
